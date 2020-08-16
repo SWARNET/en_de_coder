b962a89710dc3d5f7bd0f88fe40b36246fa67983
SWARNET <68743524+SWARNET@users.noreply.github.com>
2020-08-16 13:35:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+var letters_code =
+ "abcdefghijklmnopqrstuvwxyzabcdefghi";
+var letters_decode =
+ "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopq";
+var ans = "";
+
+function reverseString(str) {
+ var newString = "";
+ for (var i = str.length - 1; i >=
+  0; i--) {
+  newString += str[i];
+ }
+ return newString;
+}
+
+
+
+function encode() {
+ text = document.getElementById(
+  "input").value;
+ text = text.toLowerCase();
+ var i = null;
+ for (i of text) {
+
+  x = letters_code.indexOf(i) + 9;
+  if (letters_code.indexOf(i) < 0) {
+   ans += i;
+  } else {
+   ans += letters_code[x];
+  }
+
+ }
+ ans = reverseString(ans);
+ document.getElementById('display')
+  .innerHTML = ans;
+ ans = "";
+}
+
+function decode() {
+ text = document.getElementById(
+  'input').value;
+ text = text.toLowerCase();
+ var i = null;
+ for (i of text) {
+
+  x = letters_decode.indexOf(i) + 17;
+  console.log(letters_decode.indexOf(
+   i))
+  if (letters_decode.indexOf(i) <
+   0) {
+   ans += i;
+  } else {
+   ans += letters_decode[x];
+  }
+ }
+ ans = reverseString(ans);
+ document.getElementById('display')
+  .innerHTML = ans;
+ ans = "";
+
+}
+
+function reset() {
+ document.getElementById('display')
+  .innerHTML = "";
+ document.getElementById('input')
+  .value = "";
+ ans = "";
+}
